4379922b70c6fc51c5fd92ccfc02538cf2094e0e
wissalsoudani <wissal.soudani@esprit.tn>
2022-12-10 11:37:31 +0100

    project

---
+++ AM.UI.WEB/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AM.UI.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
